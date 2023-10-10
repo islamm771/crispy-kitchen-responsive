9ecdef4608e2aba7e30491067f6d6f72de202871
islamm771 <90573842+islamm771@users.noreply.github.com>
2023-10-10 11:34:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-let btnNext = document.querySelector(".next")
-let btnPrev = document.querySelector(".prev")
 let arr = ["1.jpg", "2.jpg", "3.jpg"];
 let img = document.querySelector(".slide-img");
 let index = 0;
 let state = document.querySelector(".state");
-
-
-if (btnNext != null){
-    btnNext.onclick = nextImage;
-    btnPrev.onclick = prevImage;
-}
 
 setInterval(nextImage , 3000)
 
@@ -23,19 +15,17 @@ if (datenow.getHours() > 10 && datenow.getHours() < 20) {
 }
 
 
-if (btnNext != null){
-    function nextImage() {
-      index += 1;
-      if (index >= arr.length) {
-        index = 0;
-      }
-      img.setAttribute("src", `images/slide/${arr[index]}`);
-    }
-    function prevImage() {
-      index -= 1;
-      if (index < 0) {
-        index = arr.length - 1;
-      }
-      img.setAttribute("src", `images/slide/${arr[index]}`);
-    }
+function nextImage() {
+  index += 1;
+  if (index >= arr.length) {
+    index = 0;
+  }
+  img.setAttribute("src", `images/slide/${arr[index]}`);
+}
+function prevImage() {
+  index -= 1;
+  if (index < 0) {
+    index = arr.length - 1;
+  }
+  img.setAttribute("src", `images/slide/${arr[index]}`);
 }
